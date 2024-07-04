61f983e77d0efe1be06366f8aeb5e54874c72e86
Md. Masudul Kabir <masud.ncse@gmail.com>
2024-07-04 19:50:25 +0600

    Random

--- public/settings.js
+++ public/settings.js
@@ -1,6 +1,6 @@
 
 
 window.global_ENV = {
-	// REACT_APP_API_BASE_URL: 'https://evote.apiprojects.link',
-	REACT_APP_API_BASE_URL: 'http://192.168.0.35:8000',
+	REACT_APP_API_BASE_URL: 'http://evotingcms.apisolutionsltd.com:49011',
+	// REACT_APP_API_BASE_URL: 'http://192.168.0.35:8000',
 };
